2a84c953f879a520a3e7bfc7dc60807dc51129b8
Momir Milutinović <milutinm462@gmail.com>
2024-02-13 19:20:33 +0100

    feat: Extarct list of sections of a document in scrapy spider

--- smartcat_spider.py
+++ smartcat_spider.py
@@ -7,32 +7,30 @@ from bs4 import BeautifulSoup
 class SmartcatSpider(scrapy.Spider):
     name = 'smartcat'
     start_urls = [
-        'https://smartcat.io/',
-        'https://smartcat.io/company/',
-        'https://smartcat.io/industries/',
-        'https://smartcat.io/case-studies/',
-        'https://smartcat.io/jobs/',
-        'https://smartcat.io/blog/',
-        'https://smartcat.io/tech-blog/',
+        'https://smartcat.io'
     ]
 
     def parse(self, response):
         html = response.body
         soup = BeautifulSoup(html, 'lxml')
-        self.remove_useless_elements(soup, response.url)
-        text = soup.find('div', {'id': 'page'}).get_text()
-        yield {
-            'referer': str(response.request.headers.get('Referer', None)),
-            'title': response.css('title::text').re('(.*) - SmartCat')[0],
-            'text': text
-        }
-        
+
         for link in soup.find_all("a"):
             if link["href"].startswith('https://smartcat.io'):
                 yield response.follow(link["href"], callback=self.parse)
 
+        self.remove_useless_elements(soup)
+        text = soup.find('div', {'id': 'page'}).get_text()
+        sections = [section.get_text() for section in soup.find_all('section')]
+        yield {
+            'url': response.url,
+            'title': response.css('title::text').re('(.*) - SmartCat')[0],
+            'text': text,
+            'sections': sections
+        }
+        
+
     @staticmethod
-    def remove_useless_elements(soup, url):
+    def remove_useless_elements(soup):
         if soup.header is not None:
             soup.header.decompose()
         if soup.footer is not None:
